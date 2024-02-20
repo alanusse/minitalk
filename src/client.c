2460a701310b145f6e21630efcc0628c123fe0e0
Agustín Lanusse <lanusseagustin@gmail.com>
2024-02-20 12:32:05 +0100

    testing printf

--- src/client.c
+++ src/client.c
@@ -1,4 +1,25 @@
-int main()
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/20 01:42:01 by aglanuss          #+#    #+#             */
+/*   Updated: 2024/02/20 12:20:35 by aglanuss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minitalk.h"
+
+int main(int argc, char **argv)
 {
-  return 0;
+	if (argc != 3)
+		ft_printf("Usage: client <PID> <MESSAGE>\n");
+	else
+	{
+		ft_printf("OK!\n");
+		ft_printf("%s\n", argv[2]);
+	}
+	return 0;
 }
