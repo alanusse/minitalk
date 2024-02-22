b663f271c92a23335f34db402cdf34a30ca5e2b0
Agustín Lanusse <lanusseagustin@gmail.com>
2024-02-22 02:10:19 +0100

    modified server and client

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 01:42:01 by aglanuss          #+#    #+#             */
-/*   Updated: 2024/02/20 23:34:58 by aglanuss         ###   ########.fr       */
+/*   Updated: 2024/02/22 02:01:47 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,22 @@
 
 static void	check_arguments(int argc, char **argv)
 {
+	int	i;
+
 	if (argc != 3)
 	{
 		ft_printf("Usage: client <pid> <message>\n");
 		exit(EXIT_FAILURE);
 	}
-	while (*argv[1])
+	i = 0;
+	while (argv[1][i])
 	{
 		if (!ft_isdigit(*argv[1]))
 		{
 			ft_printf("Error: invalid PID\n");
 			exit(EXIT_FAILURE);
 		}
-		argv[1]++;
+		i++;
 	}
 	if (ft_strlen(argv[2]) == 0)
 	{
@@ -35,10 +38,53 @@ static void	check_arguments(int argc, char **argv)
 	}
 }
 
+static void	send_signals(pid_t pid, char *str)
+{
+	int	tmp;
+	int	i;
+
+	while (*str)
+	{
+		i = 7;
+		while (i >= 0)
+		{
+			tmp = *str >> i;
+			if (tmp & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(11);
+			i--;
+		}
+		str++;
+	}
+}
+
+static void	send_str_len(pid_t pid, char *str)
+{
+	int	len;
+	int	i;
+
+	len = ft_strlen(str);
+	send_signals(pid, ft_itoa(len));
+	i = 0;
+	while (i < 8)
+	{
+		kill(pid, SIGUSR1);
+		usleep(11);
+		i++;
+	}
+}
+
 int main(int argc, char **argv)
 {
+	pid_t	pid;
+	char	*text;
+
+	pid = ft_atoi(argv[1]);
+	text = argv[2];
 	check_arguments(argc, argv);
-	ft_printf("OK!\n");
-	ft_printf("%s\n", argv[2]);
+	send_str_len(pid, text);
+	send_signals(pid, text);
 	return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,4 +1,30 @@
-int main()
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 22:37:41 by aglanuss          #+#    #+#             */
+/*   Updated: 2024/02/22 00:39:40 by aglanuss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minitalk.h"
+
+void	signal_handler(int signal)
 {
-  return 0;
+	if (signal == SIGUSR1)
+		ft_printf("0\n");
+	else if (signal == SIGUSR2)
+		ft_printf("1\n");
+}
+
+int	main()
+{
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+	ft_printf("✨ Started server with PID %i\n", getpid());
+	while (1)
+		sleep(1);
 }
